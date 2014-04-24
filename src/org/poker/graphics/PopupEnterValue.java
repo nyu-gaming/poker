bcaaffd96362e8a82d32251249de355d6c280b2a
maverick <shwetank.raghuvanshi@gmail.com>
2014-04-24 17:11:20 -0400

    fixed popin bug and disabled zooming

--- src/org/poker/graphics/PopupEnterValue.java
+++ src/org/poker/graphics/PopupEnterValue.java
@@ -2,29 +2,31 @@ package org.poker.graphics;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
+import com.googlecode.mgwt.dom.client.event.tap.TapEvent;
+import com.googlecode.mgwt.dom.client.event.tap.TapHandler;
+import com.googlecode.mgwt.ui.client.widget.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.googlecode.mgwt.ui.client.dialog.PopinDialog;
+import com.googlecode.mgwt.ui.client.widget.MTextBox;
 
 public class PopupEnterValue extends PopinDialog {
   public interface ValueEntered {
     void setValue(int value);
   }
   
-  final TextBox txtValue;
+  final MTextBox txtValue;
   Button btnEnter;
   
   public PopupEnterValue(String text, final ValueEntered valueEntered) {
     //super(false, true);
     //setText(text);
     
-	txtValue = new TextBox();
+	txtValue = new MTextBox();
     btnEnter = new Button("Buy-in");
-    btnEnter.addClickHandler(new ClickHandler() {
+    btnEnter.addTapHandler(new TapHandler() {
       @Override
-      public void onClick(ClickEvent event) {
+      public void onTap(TapEvent event) {
         hide();
         valueEntered.setValue(Integer.parseInt(txtValue.getText()));
       }
